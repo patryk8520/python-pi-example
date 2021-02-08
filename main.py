f20ce7e77e825d1ba01e8f37d2a89191f6669736
Patryk <patryk.lojewski92@gmail.com>
2021-02-08 20:51:15 +0100

    to samo co zwykle testy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def main(msg):
 	print(msg)
-//make some conflict
+#make some conflict fuck comments
 #some comment
 main("Hello World")
 #This is an example project to demonstrate to use the git repository
